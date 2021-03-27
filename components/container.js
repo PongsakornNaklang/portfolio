7166649a5912e69091978a454fef1ac5bd6c8bab
PongsakornNaklang <PongsakornNaklang03@gmail.com>
2021-03-28 01:45:19 +0700

    fix bg animated error

--- components/container.js
+++ components/container.js
@@ -2,10 +2,13 @@ import ThemeToggle from "./theme_toggle"
 
 const Container = ({ children }) => {
     return (
-        <div className="max-w-7xl relative mx-auto px-4 sm:px-6 lg:px-8 py-10">
-            {children}
+        <>
+            <div className="max-w-7xl relative mx-auto px-4 sm:px-6 lg:px-8 py-10">
+                {children}
+
+            </div>
             <ThemeToggle />
-        </div>
+        </>
     )
 }
 
