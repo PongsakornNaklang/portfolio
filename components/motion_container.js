23b171d72492542a6bc1fab552a4f4805028fe40
PongsakornNaklang <PongsakornNaklang03@gmail.com>
2021-03-28 01:49:04 +0700

    fix bg bug ii

--- components/motion_container.js
+++ components/motion_container.js
@@ -1,16 +1,20 @@
 import { motion } from "framer-motion"
+import ThemeToggle from "./theme_toggle"
 
-const MotionContainer = ({ children,path }) => {
+const MotionContainer = ({ children, path }) => {
     return (
-        <motion.div
-            layoutId={`${path}-svg-box`}
-            className='bg-indigo-500 dark:bg-gray-800 h-screen w-full pt-4 bg-pages'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3 }}
-        >
-            {children}
-        </motion.div>
+        <>
+            <motion.div
+                layoutId={`${path}-svg-box`}
+                className='bg-indigo-500 dark:bg-gray-800 h-screen w-full pt-4 bg-pages'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}
+            >
+                {children}
+            </motion.div>
+            <ThemeToggle />
+        </>
     )
 }
 
