330945758bc23c97ed03073757b78bb85183fb79
PongsakornNaklang <PongsakornNaklang03@gmail.com>
2021-03-28 10:25:12 +0700

    [*] fix cant download resume in vercel 6

--- pages/api/resume/download.js
+++ pages/api/resume/download.js
@@ -3,7 +3,7 @@ var mime = require('mime');
 var fs = require('fs');
 
 export default (req, res) => {
-    const dir = path.join(__dirname, 'pages', 'api', 'resume', 'resume2020.pdf');
+    const dir = path.join(__dirname, '_files', 'resume2020.pdf');
     const filename = path.basename(dir);
     const mimetype = mime.getType(dir);
     res.setHeader('Content-disposition', 'attachment; filename=' + filename);
